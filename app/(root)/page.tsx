649ebd7589e2ada536e3118634b89c26f3ea999b
looju <omofade2020@gmail.com>
2024-11-28 11:54:06 +0100

    notification system

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,20 @@
+import Headerbox from "@/components/headerbox";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Loju" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <Headerbox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subText="Access and manage your accounts and transactions"
+          />
+        </header>
+      </div>
+    </section>
+  );
 }
