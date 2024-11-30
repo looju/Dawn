a6cda47a043f95bb6380a80b40ddecf67c60cfc8
looju <omofade2020@gmail.com>
2024-11-30 19:07:28 +0100

    sidebar

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -1,38 +1,63 @@
 "use client";
 import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import { SiderbarProps } from "@/types";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 const SideBar = ({ user }: SiderbarProps) => {
   const router = useRouter();
+  const pathName = usePathname();
   return (
     <div className="sidebar">
       <nav className="flex flex-col gap-4">
         <div className="mb-12 cursor-pointer items-center gap-2">
-          <Image
-            src="/icons/logo.svg"
-            width={34}
-            height={34}
-            alt="logo"
-            className="max-xl:w-11"
-          />
-          <h1 className="sidebar-logo">Horizon</h1>
-          {sidebarLinks.map((item) => (
-            <>
-              <h2
-                onClick={(e) => {
-                  e.preventDefault();
-                  router.push(item.route);
-                }}
-                key={item.imgURL}
-                className={cn}
-              >
-                {item.label}
-              </h2>
-            </>
-          ))}
+          <div className="flex flex-row items-center">
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="logo"
+              className="max-xl:w-11"
+            />
+            <h1 className="sidebar-logo">Horizon</h1>
+          </div>
+          {sidebarLinks.map((item) => {
+            const isActive =
+              pathName === item.route || pathName.startsWith(`${item.route}/`);
+            return (
+              <>
+                <div
+                  onClick={(e) => {
+                    e.preventDefault();
+                    router.push(item.route);
+                  }}
+                  key={item.imgURL}
+                  className={cn("sidebar-link", "w-full", {
+                    "bg-bankGradient": isActive,
+                  })}
+                >
+                  <div className="relative size-6 w-full h-2 items-center flex flex-row my-2">
+                    <Image
+                      src={item.imgURL}
+                      width={20}
+                      height={20}
+                      alt="link images"
+                      className="mr-2"
+                    />
+                    <p
+                      className={cn("sidebar-label", {
+                        "!text-white": isActive,
+                      })}
+                    >
+                      {item.label}
+                    </p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
         </div>
       </nav>
     </div>
