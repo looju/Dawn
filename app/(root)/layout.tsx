8ec74e20154f0c5456bff895336ba813938725ff
looju <omofade2020@gmail.com>
2024-12-04 09:22:27 +0100

    media query

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import SideBar from "@/components/sideBar";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,17 @@ export default function RootLayout({
   return (
     <main className="flex flex-row w-full h-screen font-ibm-plex-serif">
       <SideBar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src={"/icons/logo.svg"}
+            width={30}
+            height={30}
+            alt="hambuger"
+          />
+          <div></div>
+        </div>
+      </div>
       {children}
     </main>
   );
